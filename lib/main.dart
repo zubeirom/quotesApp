1e1331aafb3cad0a1047cb6903422694492a63e2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-05-09 05:46:28 +0200

    Add card widget and create lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/quote.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,8 +13,53 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'foo', text: 'None'),
+    Quote(author: 'lok', text: 'Manno'),
+    Quote(author: 'beetle', text: 'love'),
+  ];
+
+  Widget quoteTemplate(Quote quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              )
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[800]
+              )
+            )
+          ],
+        ),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((q) => quoteTemplate(q)).toList(),
+      ),
+    );
   }
 }
